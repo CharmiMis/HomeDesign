08af5815dce6d1b8ee4810c197a3662c5824accc
Charmi Mistry <charmim.itpath@gmail.com>
2024-07-08 13:44:10 +0530

    added content

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -7,7 +7,7 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">
                             <span data-feather="home"></span>
-                            Dashboard
+                            Dashboards
                         </a>
                     </li>
                     <li class="nav-item">
