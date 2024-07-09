5e8efe7b638e40dd8fcb0ef493c139a3e602e239
Charmi Mistry <charmim.itpath@gmail.com>
2024-07-10 02:30:19 +0530

    remove and unauthecate dashboard page and profile

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,34 +1,4 @@
-<!-- resources/views/dashboard.blade.php -->
-{{-- 
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="flex">
-        <!-- Sidebar -->
-        <div class="bg-gray-800 text-white h-screen w-64">
-            <div class="p-4">
-                <ul class="mt-4">
-                    <li><a href="{{route('precision.index')}}" class="block py-2" >Precision</a></li>
-                    <!-- Add more menu items as needed -->
-                </ul>
-            </div>
-        </div>
-
-        <!-- Main Content -->
-        <div class="flex-1 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900">
-                {{ __("You're logged in!") }}
-            </div>
-        </div>
-    </div>
-</x-app-layout> --}}
-
-
-<x-app-layout>
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -45,4 +15,4 @@
             </div>
         </div>
     </div>
-</x-app-layout> 
+</x-app-layout>  --}}
--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/', [AuthenticatedSessionController::class, 'create'])
+                ->name('login');
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 
 Route::get('/precisionold', function () {
